f4f5f90cee7894644693ba87fd57d48b6d698b8c
alex <al3xback@gmail.com>
2022-02-10 20:09:52 +0700

    fix minor

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,5 @@ module.exports = {
 		},
 	},
 	publicPath:
-		process.env.NODE_ENV === 'production'
-			? '/NFT-card--Frontend-Mentor/'
-			: '/',
+		process.env.NODE_ENV === 'production' ? '/NFT--Frontend-Mentor/' : '/',
 };
